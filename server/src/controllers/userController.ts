b505bc92811c0fbd1c1520062b13600af7931c6d
Dev-Kiddo <buzzwithprasanth@gmail.com>
2026-02-04 07:56:34 +0530

    server: get users controller added

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,7 +1,19 @@
 import userModel from "../models/userModel.js";
 import { Request, Response } from "express";
 
-export const getUsers = function (request: Request, response: Response) {
-  console.log("userModel:", userModel);
-  response.send("All Users");
+export const getUsers = async function (request: Request, response: Response) {
+  const users = await userModel.find({});
+
+  if (users.length <= 0) {
+    return response.status(200).json({
+      success: true,
+      message: "User not exist yet",
+    });
+  }
+
+  return response.status(200).json({
+    success: true,
+    numOfUsers: users.length,
+    message: users,
+  });
 };
